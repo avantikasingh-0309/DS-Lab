fc735377bba9332f9c099a24feef5ef76a9eca0e
avantikasingh-0309 <avantika.singh.batch2025@sitnagpur.siu.edu.in>
2026-02-11 21:48:46 +0530

    Create Bubble sort.c

---
+++ Exp: 2/Bubble sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main() {
+	int a[20], i, n, j, temp;
+	printf("Enter value of n : ");
+	scanf("%d", &n);
+	
+	for(i=0; i<n;i++){
+		printf("Enter element for a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+	
+	printf("Before sorting the elements in the array are\n");
+	for(i=0;i<n;i++){
+		printf("Value of a[%d] = %d\n", i,a[i]);
+	}
+	
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-i-1;j++){
+			if(a[j]>a[j+1]){
+				temp = a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	
+	printf("After sorting the elements in the array are\n");
+	for(i=0;i<n;i++){
+		printf("Value of a[%d] = %d\n", i,a[i]);
+	}
+	
+}
